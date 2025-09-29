1077fa6eefc589f7a5197e3b4d06cc9f37c7e536
zaymoeyan <zaymoeyan21@gmail.com>
2025-09-29 19:13:41 +0630

    update login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+          <link rel="icon" href="/icon.ico" sizes="32*32"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
