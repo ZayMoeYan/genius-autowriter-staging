3d0928c1113c6ca41e7f296677a67c7038ffb9c4
zaymoeyan <zaymoeyan21@gmail.com>
2025-10-08 12:10:21 +0630

    update

--- src/app/actions/logoutAction.ts
+++ src/app/actions/logoutAction.ts
@@ -6,8 +6,8 @@ export async function logout() {
 
     const cookieStore = await cookies();
     cookieStore.delete('access-token');
-    cookieStore.delete('api-token');
-    cookieStore.delete('role');
-    cookieStore.delete('username');
-    cookieStore.delete('email');
+    cookieStore.delete('apikey-token');
+    cookieStore.delete('role_token');
+    cookieStore.delete('username_token');
+    cookieStore.delete('email_token');
 }
