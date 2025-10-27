c4d18faf97b3ede91ce2fb3b9b4f970e3efed45e
zaymoeyan <zaymoeyan21@gmail.com>
2025-10-27 11:18:38 +0630

    update UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,20 @@
 "use client";
 
-import {useEffect, useRef, useState} from "react";
+import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
-import { useRouter} from "next/navigation";
-
 import motLogo from '@/app/images/MOT.png';
 import LanguageSwitcher from "@/components/LanguageSwitcher";
-import {useAuthStore} from "@/stores/useAuthStore";
 
 
 
 export const Nav = () => {
-    const router = useRouter();
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <nav className="shadow-lg w-full bg-black text-white fixed z-10 border-red-800 border-b-[1.5px]">
 
-            <div className="flex flex-row py-4 justify-between w-[90%] max-w-7xl items-center mx-auto">
+            <div className="flex flex-row py-4 justify-between w-[90%] max-w-7xl items-center mx-auto xl:max-w-full">
 
                 <div className="flex items-center space-x-3 cursor-pointer">
                     <img src={motLogo.src} alt="MOT Logo" className="h-10 w-auto" />
